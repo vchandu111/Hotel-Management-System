c6515f970e9bee2f8873cccb560e5663581873e2
Aswani Gedela <gakhila281994@gmail.com>
2020-07-16 12:41:54 +0530

    Admin details

--- signin.js
+++ signin.js
@@ -2,8 +2,8 @@ var login = document.getElementById( 'login' )
 
 login.addEventListener( 'click', () => {
     event.preventDefault()
-    const name = document.getElementById( 'username' ).value
-    const password = document.getElementById( 'exampleInputPassword1' ).value
+    let name = document.getElementById( 'username' ).value
+    let password = document.getElementById( 'exampleInputPassword1' ).value
     let userInfo = {}
     userInfo.username = name
     userInfo.password = password
@@ -12,13 +12,21 @@ login.addEventListener( 'click', () => {
     registeredUsers = JSON.parse( registeredUsers )
     //console.log( userInfo, registeredUsers )
 
-    for ( let index = 0; index < registeredUsers.length; index++ ) {
-        if ( userInfo.username == registeredUsers[ index ].Username && userInfo.password == registeredUsers[ index ].Password ) {
-            location.href = 'rooms.html'
-            break
+    //=========== Admin Login ==============
+
+    if ( name == 'akki' && password == 'chandu' ) {
+        location.href = 'admin.html'
+    }
+    else {
+        let index = 0;
+        while ( index < registeredUsers.length ) {
+            if ( name == registeredUsers[ index ].Username && password == registeredUsers[ index ].Password ) {
+                location.href = 'rooms.html'
+                break
+            }
+            alert( 'Incorrect username or password' )
+            index++
         }
     }
-
-    alert( 'Incorrect username or password' )
-
+    name.textContent = ''
 } )
